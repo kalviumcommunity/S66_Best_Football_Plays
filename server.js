c2001a08ac6ff8d64846f1c76a2638acbbeba15d
iiuperboyii <aaronjomon24@gmail.com>
2025-01-29 09:50:58 +0530

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express=require("express")
-const app=express();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require('./model/user.model');
+require('dotenv').config();
 
-const PORT=8989;
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Successfully connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
 
 app.get("/ping",(req,res)=>{
       try {
@@ -13,6 +25,24 @@ app.get("/ping",(req,res)=>{
       }
 })
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on http://localhost:${PORT} successfully`);
+
+
+app.post('/create', async(req,res)=>{
+  const{username,password} = req.body;
+  payload={username,password};
+  
+  try {
+      let new_user = new UserModel(payload);
+      await new_user.save();
+      res.send({ "message": "Hurray! Successfully saved the user to the database" });
+  } catch (error) {
+      console.log(error);
+      res.send({ "error": error });
+  }
+});
+
+
+
+app.listen(process.env.PORT,()=>{
+  console.log(`Server is running on http://localhost:${process.env.PORT} successfully`);
 })
